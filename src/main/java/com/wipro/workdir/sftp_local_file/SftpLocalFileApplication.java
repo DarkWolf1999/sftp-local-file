89a4b539363f2179db79e3835d03fd679b5fe45a
mayur <mayurraut166199@gmail.com>
2026-09-15 13:28:17 +0530

    comment added

--- src/main/java/com/wipro/workdir/sftp_local_file/SftpLocalFileApplication.java
+++ src/main/java/com/wipro/workdir/sftp_local_file/SftpLocalFileApplication.java
@@ -3,6 +3,7 @@ package com.wipro.workdir.sftp_local_file;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Extra comment is added
 @SpringBootApplication
 public class SftpLocalFileApplication {
 	public static void main(String[] args) {
